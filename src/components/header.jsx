dd893ae9b91f50f73a21db825ca470e4643c3080
Alex <lexusotoo@gmail.com>
2026-05-05 11:47:19 +0000

    begun with footer and header

---
+++ src/components/header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <div className='header-logo'></div>
+        <div>
+            <span>Research</span>
+            <span>Blog</span>
+            <span>Services</span>
+            <span>Contact Us</span>
+        </div>
+
+      
+    </header>
+  )
+}
+
+export default Header
